eadab55abc3a92072d0d7321befd2f34cfb9e76a
Santiago Navarro Cuy <148833934+Tw3kda@users.noreply.github.com>
2025-10-31 18:37:17 -0500

    Update apolloClient.js

--- frontend/src/components/apolloClient.js
+++ frontend/src/components/apolloClient.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-const GRAPHQL_ENDPOINT = import.meta.env.VITE_GRAPHQL_URL || 'http://localhost:4000/';
+const GRAPHQL_ENDPOINT = import.meta.env.VITE_GRAPHQL_URL || 'http://localhost:8080/';
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: GRAPHQL_ENDPOINT }),
