bb07af5ef8a0feca2d930bc677e8f1929902a212
Tw3kda <santiago2006nc@gmail.com>
2025-10-31 19:23:50 -0500

    fix

--- frontend/src/components/apolloClient.js
+++ frontend/src/components/apolloClient.js
@@ -1,6 +1,7 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-const GRAPHQL_ENDPOINT = import.meta.env.VITE_GRAPHQL_URL || 'http://localhost:8080/';
+const GRAPHQL_ENDPOINT =
+  import.meta.env.VITE_GRAPHQL_URL || "http://localhost:4000";
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: GRAPHQL_ENDPOINT }),
